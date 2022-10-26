185f60ef935c3f09a0a46b78b84a7e8a4f3c31eb
sshettyn <ShettyS_P@boqdev.com.au>
2022-10-26 14:47:38 +0530

    Initial commit

---
+++ app/src/main/java/com/deloitte/androidbasics/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.deloitte.androidbasics
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+//4 components - activity,service,broadcast reciever,content provider
+
+class MainActivity : AppCompatActivity() {
+
+  //1.Linear layout
+  //2.Constraint layout
+  //3.Relative layout
+
+  private lateinit var clickButton: Button
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    clickButton = findViewById(R.id.bt1)
+    clickButton.setOnClickListener {
+      val intent  = Intent(this,MainActivity2::class.java)
+      startActivity(intent)
+    }
+  }
+
+}
